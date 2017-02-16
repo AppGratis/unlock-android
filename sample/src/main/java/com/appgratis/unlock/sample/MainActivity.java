394ee2c2e759c121e967a36422ae767398a1cef7
Arnaud Barisain-Monrose <arnaud@batch.com>
2017-02-16 18:26:34 +0100

    Start working on the sample

--- sample/src/main/java/com/appgratis/unlock/sample/MainActivity.java
+++ sample/src/main/java/com/appgratis/unlock/sample/MainActivity.java
@@ -27,12 +27,72 @@ package com.appgratis.unlock.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.DividerItemDecoration;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainAdapter.SectionDataProvider {
+
+    RecyclerView recyclerView;
+    MainAdapter recyclerViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewAdapter = new MainAdapter(this);
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                LinearLayoutManager.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    @Override
+    public int getSectionCount() {
+        return 3;
+    }
+
+    @Override
+    public int getItemCount(int section) {
+        switch (section) {
+            case 0:
+                return 3;
+            case 1:
+                return 3;
+            case 2:
+                return 3;
+        }
+        return 0;
+    }
+
+    @Override
+    public String getHeaderText(int section) {
+        switch (section) {
+            case 0:
+                return "Available Offers";
+            case 1:
+                return "Redeemed Features";
+            case 2:
+                return "Resources";
+        }
+        return "";
+    }
+
+    @Override
+    public void onBindViewHolder(MainAdapter.ItemViewHolder holder, int section, int row) {
+        holder.titleTextView.setText("Foo");
+        holder.detailsTextView.setText("Bar");
+        switch (section) {
+            case 0:
+                return;
+            case 1:
+                return;
+            case 2:
+                return;
+        }
     }
 }
