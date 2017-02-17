776fb86f8f1bb2bef4e0086b663f53c8dad98728
Arnaud Barisain-Monrose <arnaud@batch.com>
2017-02-17 15:18:52 +0100

    Lay down the groundwork for the sample list

---
+++ sample/src/main/java/com/appgratis/unlock/sample/AvailableOffersDatasource.java
@@ -0,0 +1,31 @@
+package com.appgratis.unlock.sample;
+
+import android.support.annotation.NonNull;
+
+import com.appgratis.unlock.model.Offer;
+
+import java.util.List;
+
+public class AvailableOffersDatasource implements SectionedMetaAdapter.SectionDatasource {
+
+    private List<Offer> availableOffers;
+
+    public AvailableOffersDatasource(@NonNull List<Offer> availableOffers) {
+        this.availableOffers = availableOffers;
+    }
+
+    @Override
+    public int getItemCount() {
+        return availableOffers.size();
+    }
+
+    @Override
+    public String getHeaderText() {
+        return "Available Offers";
+    }
+
+    @Override
+    public void onBindViewHolder(SectionedMetaAdapter.ItemViewHolder holder, int row) {
+
+    }
+}
